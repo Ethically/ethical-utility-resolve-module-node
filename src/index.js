2f57be8dd3500ff72c83b3c4555ffc4e4b806067
Marceli.no <me@marceli.no>
2017-10-30 01:31:38 -0400

    [feat] Support the browser package.json spec
    Spec: https://github.com/defunctzombie/package-browser-field-spec
    
    [fix] Ensure version conflicts are resolved correctly
    
    Currently, the module loader does not distinguish between different
    versions of the same module, which causes inconsistencies in the
    vendor's API, leading to runtime errors. One solution is to supply
    the conflicted modules.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,182 @@
+import clone from 'clone'
+import { pathExistsSync } from 'fs-extra'
 import { readFileSync } from 'fs'
 import { join, resolve, dirname } from 'path'
-import { isAbsolute, getRootPath } from 'ethical-utility-path'
-import { appendExtension, getAppPrefix } from 'ethical-utility-resolve-module'
+import { isAbsolute, isRelative, getRootPath } from 'ethical-utility-path'
+import {
+    appendExtension,
+    getAppPrefix,
+    isAbsolutePackage,
+    isRelativePackage,
+    isAppModule
+} from 'ethical-utility-resolve-module'
 
-export * from 'ethical-utility-resolve-module'
+const cache = {}
+const browserMap = {}
+const conflictMap = {}
 
-export const resolveNodeModuleFile = (file) => {
-    const path = join(getNodeModulesPath(), file)
+export const getConflictMap = () => {
+    return clone(conflictMap)
+}
+
+export const isConflicted = (request, parent) => {
+    if (isRelative(request)) {
+        return
+    }
+    const id = getModuleRootID(parent)
+    const found = conflictMap[id] && conflictMap[id][request]
+    if (found) {
+        return found
+    }
+    resolveModulePath(request, parent)
+    return conflictMap[id] && conflictMap[id][request]
+}
+
+const isPathConflicted = (path) => {
+    return !!((path.match(/\/node_modules\//g) || []).length > 1)
+}
+
+const mapConflictDependecies = (request, parent, path) => {
+    if (isRelative(request)) {
+        return
+    }
+    const id = getModuleRootID(parent)
+    if (!conflictMap[id]) {
+        conflictMap[id] = {}
+    }
+    conflictMap[id][request] = generateModuleID(path)
+}
+
+const mapBrowserDependecies = (browser, parent) => {
+
+    const id = getModuleRootID(parent)
+    if (browserMap[id]) {
+        return
+    }
+
+    const map = {}
+    for (const request in browser) {
+        const left = resolveBrowserRemap(request, parent)
+        const right = resolveBrowserRemap(browser[request], parent)
+        map[left] = right
+    }
+    if (map) {
+        browserMap[id] = map
+    }
+}
+
+export const getBrowserMap = () => {
+    return clone(browserMap)
+}
+
+export const isRemapped = (request, parent) => {
+    if (!parent) {
+        return
+    }
+
+    const scope = getModuleRootID(parent)
+    const key = resolveBrowserRemap(request, parent)
+    const remapped = browserMap[scope] && browserMap[scope][key]
+    if (remapped) {
+        return remapped
+    }
+}
+
+export const getModuleRootID = (module) => {
+    return generateModuleID(getNodeModuleRoot(module))
+}
+
+const resolveBrowserRemap = (request, parent) => {
+    if (!request) {
+        return 'ethical-noop-module-empty/index.js'
+    }
+    if (isAbsolutePackage(request)) {
+        return request
+    }
+    return generateModuleID(resolveModulePath(request, parent))
+}
+
+export const resolveRelative = (request, parent) => {
+    return appendExtension(resolve(dirname(parent), request))
+}
+
+export const resolveNodeModuleFile = (request, parent) => {
+    return appendExtension(join(getNodeModulesPath(), request))
+}
+
+const findPackageJSON = (name, parent = '') => {
+    const packageJSON = name + '/package.json'
+    let directories = parent.split('/')
+    let index
+    const nodeModules = 'node_modules'
+    while ((index = directories.lastIndexOf(nodeModules)) > -1) {
+        const parentRoot = directories.slice(0, index + 2).join('/')
+        const dependency = parentRoot + '/'  + nodeModules + '/' + packageJSON
+        if (pathExistsSync(dependency)) {
+            return dependency
+        }
+        directories = directories.slice(0, index - 1)
+    }
+    return getNodeModulesPath() + '/' + packageJSON
+}
+
+const resolveBrowserFile = (packageJSON, packageJSONPath) => {
+    const { name, main, browser } = packageJSON
+    if (typeof browser === 'object') {
+        mapBrowserDependecies(browser, packageJSONPath)
+    } else if (typeof browser === 'string') {
+        return browser
+    } else {
+        console.warn(
+            `The imported package (${name}) appears to not be a browser ` +
+            'ready module and might behave unexpectedly if it attempts to ' +
+            'use functionality that is exclusive to the Node.js environment.'
+        )
+    }
+    if (typeof main === 'string') {
+        return main
+    }
+    return 'index.js'
+}
+
+const getPackageJSON = (packageJSONPath) => {
+    if (cache[packageJSONPath]) {
+        return cache[packageJSONPath]
+    }
+    return cache[packageJSONPath] = JSON.parse(readFileSync(packageJSONPath))
+}
+
+const resolveMainFile = (name, parent) => {
+    const packageJSONPath = findPackageJSON(name, parent)
+    const packageJSON = getPackageJSON(packageJSONPath)
+    const packageRoot = dirname(packageJSONPath)
+    return join(packageRoot, resolveBrowserFile(packageJSON, packageJSONPath))
+}
+
+export const resolveNodeModule = (request, parent) => {
+    const path = resolveMainFile(request, parent)
+
+    if (isPathConflicted(path)) {
+        mapConflictDependecies(request, parent, path)
+    }
     return appendExtension(path)
+}
+
+export const resolveModulePath = (request, parent) => {
+    if (isAbsolutePackage(request)) return resolveNodeModule(request, parent)
+    if (isRelativePackage(request)) return resolveNodeModuleFile(request)
+    if (isRelative(request)) return resolveRelative(request, parent)
+    return request
+}
+
+export const getNodeModulesPath = () => (
+    join(getRootPath(), 'node_modules')
+)
+
+export const getNodeModuleRoot = (path) => {
+    const nodeModules = 'node_modules'
+    const parts = path.split('/')
+    return parts.slice(0, parts.lastIndexOf(nodeModules) + 2).join('/')
 }
 
 export const generateModuleID = (path) => {
@@ -16,26 +185,4 @@ export const generateModuleID = (path) => {
         return path.replace(getRootPath(), getAppPrefix())
     }
     return id
-}
-
-export const getNodeModulesPath = () => (
-    join(getRootPath(), 'node_modules')
-)
-
-export const resolveRelative = (a, b) => {
-    return appendExtension(resolve(dirname(b), a))
-}
-
-export const resolveNodeModule = (name) => {
-    const module = `${getNodeModulesPath()}/${name}/package.json`
-    const { main, browser, files } = JSON.parse(readFileSync(module))
-    const path = join(getNodeModulesPath(), name, browser || main || files[0])
-    if (!browser) {
-        console.warn(
-            `The imported package (${name}) appears to not be a browser ` +
-            'ready module and might behave unexpectedly if it attempts to ' +
-            'use functionality that is exclusive to the Node.js environment.'
-        )
-    }
-    return appendExtension(path)
 }
--- test/specs/index.js
+++ test/specs/index.js
@@ -1,11 +1,21 @@
+import requireHacker from 'require-hacker'
+import { readFileSync } from 'fs'
 import { join } from 'path'
+import chai from 'chai'
+import spies from 'chai-spies'
 import {
     getNodeModulesPath,
     resolveRelative,
     resolveNodeModule,
     resolveNodeModuleFile,
-    generateModuleID
+    resolveModulePath,
+    generateModuleID,
+    getBrowserMap,
+    getConflictMap
 } from '../../src/index.js'
+
+const { expect } = chai
+chai.use(spies)
 
 const appRoot = process.cwd()
 const nodeModulesPath = join(appRoot, '/node_modules/')
@@ -15,55 +25,83 @@ const appNodeModule = join(nodeModulesPath, 'app', 'index.js')
 
 describe('generateModuleID()', () => {
     it('should transform absolute app file', () => {
-        expect(generateModuleID(appModule)).toBe('~/app.js')
+        expect(generateModuleID(appModule)).to.equal('~/app.js')
     })
     it('should transform absolute node app file', () => {
-        expect(generateModuleID(appNodeModule)).toBe('app/index.js')
+        expect(generateModuleID(appNodeModule)).to.equal('app/index.js')
     })
 })
 
 describe('resolveRelative()', () => {
     it('should resolve a file relative to another', () => {
         expect(resolveRelative('./relative.js', appModule))
-        .toBe(appModule.replace('app.js', 'relative.js'))
+        .to.equal(appModule.replace('app.js', 'relative.js'))
     })
     it('should append extension', () => {
         expect(resolveRelative('./relative', appModule))
-        .toBe(appModule.replace('app.js', 'relative.js'))
+        .to.equal(appModule.replace('app.js', 'relative.js'))
     })
 })
 
 describe('resolveNodeModule()', () => {
+    it('should resolve modules with version conflicts', () => {
+        const module = 'ethical-noop-module-conflict'
+        const hacker = requireHacker.global_hook('*', (module, parent) => {
+            if (module.endsWith('.json')) {
+                return
+            }
+            const { id } = parent
+            const path = resolveModulePath(module, id)
+            const source = readFileSync(path, 'utf8')
+            return { source, path }
+        })
+        const result = require(module)
+        const conflictMap = {
+            "ethical-noop-module-conflict": {
+                "ethical-noop-module-conflict-sub": "ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub/index.js"
+            },
+            "ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub": {
+                "ethical-noop-module-conflict": "ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub/node_modules/ethical-noop-module-conflict/index.js"
+            },
+            "ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub/node_modules/ethical-noop-module-conflict": {
+                "ethical-noop-module-conflict-sub": "ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub/node_modules/ethical-noop-module-conflict/node_modules/ethical-noop-module-conflict-sub/index.js"
+            }
+        }
+        expect(result).to.equal('Bottom of the rabbit hole!')
+        expect(getConflictMap()).to.deep.equal(conflictMap)
+        hacker.unmount()
+    })
+    it('should resolve a node module to its browser entry file', () => {
+        const module = 'ethical-noop-module-browser-spec'
+        resolveNodeModule(module)
+        const browserMap =  {
+            'ethical-noop-module-browser-spec': {
+                'ethical-noop-module-node': 'ethical-noop-module-browser',
+                'ethical-noop-module-one': 'ethical-noop-module-browser-spec/browser.js',
+                'ethical-noop-module-browser-spec/remap.js': 'ethical-noop-module-two',
+                'ethical-noop-module-browser-spec/node/file.js': 'ethical-noop-module-browser-spec/browser/file.js',
+                'ethical-noop-module-browser-spec/noop.js': 'ethical-noop-module-empty/index.js'
+            }
+        }
+        expect(getBrowserMap()).to.deep.equal(browserMap)
+    })
     it('should resolve a node module to its browser entry file', () => {
         const module = 'ethical-noop-module-browser'
         const entry = join(nodeModulesPath, module, '/browser.js')
-        expect(resolveNodeModule(module)).toBe(entry)
+        expect(resolveNodeModule(module)).to.equal(entry)
     })
     it('should resolve node entry file and warm user of consequences', () => {
         const module = 'ethical-noop-module-node'
         const entry = join(nodeModulesPath, module, '/index.js')
         const originalConsoleWarn = console.warn
-        const consoleWarnSpy = jasmine.createSpy('console.warn')
+        const consoleWarnSpy = chai.spy('console.warn')
         console.warn = (text) => {
             consoleWarnSpy()
-            expect(text.length).toBeGreaterThan(0)
+            expect(text.length).to.be.above(0)
         }
-        expect(resolveNodeModule(module)).toBe(entry)
+        expect(resolveNodeModule(module)).to.equal(entry)
         console.warn = originalConsoleWarn
-        expect(consoleWarnSpy).toHaveBeenCalled()
-    })
-    it('should resolve node entry file out of "files" and warm user of consequences', () => {
-        const module = 'ethical-noop-module-files'
-        const entry = join(nodeModulesPath, module, '/files.js')
-        const originalConsoleWarn = console.warn
-        const consoleWarnSpy = jasmine.createSpy('console.warn')
-        console.warn = (text) => {
-            consoleWarnSpy()
-            expect(text.length).toBeGreaterThan(0)
-        }
-        expect(resolveNodeModule(module)).toBe(entry)
-        console.warn = originalConsoleWarn
-        expect(consoleWarnSpy).toHaveBeenCalled()
+        expect(consoleWarnSpy).to.have.been.called()
     })
 })
 
@@ -71,6 +109,6 @@ describe('resolveNodeModuleFile()', () => {
     it('should resolve a relative node module file', () => {
         const moduleFile = join('ethical-noop-module-browser', 'browser.js')
         const resolvedFile = join(nodeModulesPath, moduleFile)
-        expect(resolveNodeModuleFile(moduleFile)).toBe(resolvedFile)
+        expect(resolveNodeModuleFile(moduleFile)).to.equal(resolvedFile)
     })
 })
